25989b76500437d9fa7f62538a668cd17705fe6a
Wilson Wong <wilwong89@gmail.com>
2020-01-14 22:52:47 +0000

    yes

--- spinner2.js
+++ spinner2.js
@@ -1,35 +1,10 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 1500);
-
-// ... fill in the rest yourself ...
+const animation = ['|','/','-','\\'];
+for (let i = 0; i <= 10; i++) {
+  let delay = ((i * 100) + 200);
+  setTimeout(() => {
+    process.stdout.write('\r' + animation[i % 3]);
+    if (i === 10) {
+      process.stdout.write('\n\r');
+    }
+  }, delay);
+};
